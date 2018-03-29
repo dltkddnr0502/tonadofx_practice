763caf83562452b78eea63465b7205e9ebd560a1
Sangwook <sangwook.david.lee@sk.com>
2018-03-29 18:15:57 +0900

    Initial commit

---
+++ src/main/kotlin/com/ddnr/find11st/Find11stApplication.kt
@@ -0,0 +1,11 @@
+package com.ddnr.find11st
+
+import com.ddnr.MyView
+import tornadofx.App
+
+/**
+ * https://edvin.gitbooks.io/tornadofx-guide/content/part1/3.%20Components.html
+ * https://github.com/thomasnield/rxkotlinfx-tornadofx-demo
+ * https://github.com/dltkddnr0502/tonadofx_practice
+ * */
+class Find11stApplication: App(MyView::class)
